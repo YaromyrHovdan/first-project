665cce2c577d575049507ef3fdaab27353113bfd
YaromyrHovdan <yaromyr.hovdan@gmail.com>
2025-07-03 20:22:06 +0300

    nums

---
+++ nums.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int  num1 = 3;
+    int num2 = 8;
+    if (num1 > num2){
+        cout<<num1<<" is bigger than "<<num2; 
+        }
+    else {
+        cout<<num2<<" is bigger than "<<num1;
+        }    
+    return 0;
+    }
---
+++ tg.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numbers[] = {3,-9,23,45,2,-12,-99,7};
+    cout<< " enter number"<<endl;
+    cin>> num;
+    if (){
+        
+        
+        }
+    return 0;
+    }
