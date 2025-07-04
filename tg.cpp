c9eba1e26abde2f4db1a2497a86493dc6ccfe137
YaromyrHovdan <yaromyr.hovdan@gmail.com>
2025-07-04 09:16:01 +0300

    yay

--- tg.cpp
+++ tg.cpp
@@ -3,11 +3,23 @@ using namespace std;
 
 int main(){
     int numbers[] = {3,-9,23,45,2,-12,-99,7};
-    cout<< " enter number"<<endl;
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    cout<< " enter number " <<endl;
+    int num;
     cin>> num;
-    if (){
-        
-        
+
+    // If the number is found, return the index of the number
+    // If the number is found multiple times, return the index of the first occurrence
+    int index = -1;
+    for (int i = 0;i < size; i++){
+        if (numbers[i] == num){
+            index = i;
+            break;
+            }
         }
+    if (index != -1) {
+        cout << "Number found at index: " << index << endl;
+    } else {
+        cout << "Number not found" << endl;
     return 0;
     }
